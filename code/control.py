aae4acb6da083185b2e501eff3bb4ade0dbf9e0c
The-Tobster <mearst097@gmail.com>
2025-10-06 19:02:45 -0500

    Update control.py

--- code/control.py
+++ code/control.py
@@ -2,7 +2,8 @@ import socket
 import RPi.GPIO as GPIO
 import time
 import json
-from picamera2 import Picamera2, Preview
+from picamera2 import Picamera2
+import subprocess
 import pigpio
 
 # GPIO setup
@@ -50,13 +51,21 @@ PORT = 65432
 
 # camera start
 picam2 = Picamera2()
-picam2.set_controls({"ScalerCrop": (0, 0, 1920, 1080)})
-config = picam2.create_preview_configuration(
-    main={"size": (160, 120)}
-)
-picam2.configure(config)
-picam2.start_preview(Preview.QTGL)
+picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
 picam2.start()
+
+ip = "192.168.1.10"  # replace with your Windows PC's IP
+
+cmd = [
+    "gst-launch-1.0", "v4l2src", "!",
+    "video/x-raw,width=640,height=480,framerate=30/1", "!",
+    "videoconvert", "!",
+    "x264enc", "tune=zerolatency", "bitrate=800", "speed-preset=superfast", "!",
+    "rtph264pay", "!",
+    f"udpsink", f"host={ip}", "port=5000"
+]
+
+subprocess.Popen(cmd)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
